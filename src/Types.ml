bd4dc33bcbee1b1980df9835c8c2fe1011b5508e
nicolas-dalcorso <rdalcorso.nicolas@gmail.com>
2025-12-06 16:46:13 -0300

    documenta a sintaxe de tipos

--- src/Types.ml
+++ src/Types.ml
@@ -1,35 +1,29 @@
 (**
-  `src/Types.ml`
-  
-  Definição do sistema de tipos para a linguagem `L2`
+    `src/Types.ml`
+    
+    Definição do sistema de tipos para a linguagem `L_2`
+
+    `ErrorType` não é um tipo na linguagem `L_2`. Ao invés disso,
+    este tipo representa a ocorrência de um erro durante a inferência
+    estática de tipos. O programador de `L_2` não deve ter acesso a
+    `ErrorType`.
 *)
 
-(** Sistema de tipos de L2 *)
-type tipo   = ..;;
 
-(** O sistema de tipos é declarado extensível pois, em `typeinfer.ml`,
-será definido o tipo-erro para a inferência de tipos.*)
-type  tipo  +=
-    | Unit                    (* unit *)
-    | Int                     (* número inteiro *)
-    | Bool                    (* booleano *)
-    | Reference of tipo       (* referência de tipos *)
+(** sistema de tipos para a linguagem `L_2` *)
+type  tipo  =
+    | Unit                      (* unit *)
+    | Int                       (* número inteiro *)
+    | Bool                      (* booleano *)
+    | Reference of tipo         (* referência de tipos *)
+    | ErrorType of string       (* erro de tipagem *)
 ;;
 
+
+(** verifica se `t1` e `t2` são tipos equivalentes *)
 let rec eq_tipo (t1: tipo) (t2: tipo) : bool = match t1, t2 with
     | Unit, Unit | Int, Int | Bool, Bool -> true
     | Reference t1, Reference t2 -> eq_tipo t1 t2
     | _ -> false
 ;;
 
-(**
-    A inferência estática de tipos extende o sistema de tipos de `L2`
-    para incluir um tipo erro, `ErrorType`. Isso garante a totalidade
-    ou completude trivial do algoritmo: para todo termo `e`, `typeinfer e`
-    retornará um tipo. Isto é diferente do que dizer que se `e` é tipável,
-    então `typeinfer e` será capaz de derivar o tipo de `e`.
-
-    `ErrorType` não deve ser um tipo acessível ao programador.
-**)
-type tipo += 
-    | ErrorType of string;;
