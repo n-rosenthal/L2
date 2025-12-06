82dc6123e0e16b8bfedfcbb26cccf19a6f5eceac
nicolas-dalcorso <rdalcorso.nicolas@gmail.com>
2025-12-06 17:38:09 -0300

    documenta a inferẽncia estática de tipos

--- src/TypeInference.ml
+++ src/TypeInference.ml
@@ -136,6 +136,14 @@ let infer (e: term) : tipo * type_inference = (
             } :: r)
         )
 
+        (** Identifier x, variáveis
+            O tipo de um identificador `x` é o tipo associado a ele no ambiente de tipos `env`. 
+
+                Γ(x) = t => Γ ⊢ x : t
+            
+            Se `x` não estiver no ambiente de tipos `env`, então `Identifier x` : TypeError
+            é gerado um erro de variável não declarada.
+        *)
         | Identifier x -> (
             (match lookup x env with
                 | Some t -> (t, env, {
@@ -147,14 +155,24 @@ let infer (e: term) : tipo * type_inference = (
                     post = string_of_env env ^ " ⊢ '" ^ ast_of_term e ^ "' : " ^ string_of_tipo t;
                     } :: r)
 
-                | None -> (ErrorType ("UnboundIdentifier"), env, {
-                    name = "T-Var Error UnboundIdentifier";
+                | None -> (ErrorType ("Identificador " ^ x ^ " não declarado sobre o ambiente " ^ string_of_env env), env, {
+                    name = "T-Var Error Variável não declarada";
                     pre = "'" ^ x ^ "' ∉ Γ";
                     post = string_of_env env ^ " ⊢ '" ^ ast_of_term e ^ "' : " ^ string_of_tipo (ErrorType ("UnboundIdentifier"));
                     } :: r)
                 )
             )
         
+
+        (** if e1 then e2 else e3, operador condicional
+            O tipo de um operador condicional `if e1 then e2 else e3` será o tipo de um de seus branches (e2 ou e3).
+            Em L_2, é necessário que os tipos do then (`e2`) e do else (`e3`) sejam iguais, então
+
+                se `e1` : Bool, e `e2` e `e3` : t, então Γ ⊢ if e1 then e2 else e3 : t
+            
+            Se `e1` não for uma expressão booleana, então um erro é levantado;
+            Se os tipos de `e2` e `e3` não forem iguais, então um erro é levantado.
+        *)
         | Conditional (e1, e2, e3) -> (
             let t1, env',   r1 = infer' e1 env r in
             let t2, env'',  r2 = infer' e2 env' r1 in
@@ -180,10 +198,43 @@ let infer (e: term) : tipo * type_inference = (
             )
         )
 
+        (** operações binárias (BinaryOperation (bop, e1, e2)) 
+            O tipo de uma operação binária `e1 bop e2` depende do operador `bop` e dos tipos de `e1` e `e2`.
+            Para as operações binárias artiméticas
+                bop =   Add, Sub, Mul, Div
+                e1  :   Int
+                e2  :   Int
+                
+                =>  (bop e1 e2) : Int
+            
+            para as operações binárias aritméticas relacionais
+                bop =   Eq, Neq, Gt, Geq, Lt, Leq
+                e1  :   Int
+                e2  :   Int
+                
+                =>  (bop e1 e2) : Bool
+                
+            para as operações binárias lógicas relacionais
+                bop =   Eq, Neq
+                e1  :   Bool
+                e2  :   Bool
+                
+                =>  (bop e1 e2) : Bool
+            
+            e para as operações binárias lógicas somente
+                bop =   And, Or
+                e1  :   Bool
+                e2  :   Bool
+                
+                =>  (bop e1 e2) : Bool
+        
+            quaisquer outras configurações, que não estas, geram erro de tipo `ErrorType`.
+            *)
         | BinaryOperation (op, e1, e2) -> (
             let t1, env',  r1 = infer' e1 env r in
             let t2, env'', r2 = infer' e2 env' r1 in
             (match op, t1, t2 with
+                (** op. binárias aritméticas (bop, Int, Int) : Int *)
                 | Add, Int, Int 
                 | Sub, Int, Int
                 | Mul, Int, Int ->
@@ -205,6 +256,7 @@ let infer (e: term) : tipo * type_inference = (
                         } :: r2)
                 )
 
+                (** op. binárias aritméticas relacionais (bop, Int, Int) : Bool *)
                 | Eq, Int, Int
                 | Neq, Int, Int
                 | Lt, Int, Int
@@ -217,6 +269,7 @@ let infer (e: term) : tipo * type_inference = (
                         post = string_of_env env'' ^ " ⊢ '" ^ ast_of_term e ^ "' : Bool";
                         } :: r2)
 
+                (** op. binárias lógicas relacionais (bop, Bool, Bool) : Bool *)
                 | Eq, Bool, Bool
                 | Neq, Bool, Bool ->
                     (Bool, env'',  {
@@ -225,6 +278,7 @@ let infer (e: term) : tipo * type_inference = (
                         post = string_of_env env'' ^ " ⊢ '" ^ ast_of_term e ^ "' : Bool";
                         } :: r2)
 
+                (** op. binárias lógicas (bop, Bool, Bool) : Bool *)
                 | And, Bool, Bool
                 | Or, Bool, Bool ->
                     (Bool,  env'',  {
@@ -233,6 +287,7 @@ let infer (e: term) : tipo * type_inference = (
                         post = string_of_env env'' ^ " ⊢ '" ^ ast_of_term e ^ "' : Bool";
                         } :: r2)
 
+                (** erro *)
                 | _ -> (ErrorType ("Operador binário inválido inválido\n\t[" ^ string_of_env env ^ "]"),  env'',  {
                     name = "T-Op" ^ string_of_binary_operator op ^ " Error";
                     pre = string_of_env env'' ^ " ⊢ '" ^ ast_of_term e1 ^ "' : " ^ string_of_tipo t1 ^ " ∧ '" ^ ast_of_term e2 ^ "' : " ^ string_of_tipo t2;
@@ -241,6 +296,15 @@ let infer (e: term) : tipo * type_inference = (
             )
         )
 
+        (** while e1 do e2 end 
+            e1 : Bool ∧ e2 : Unit => while e1 do e2 : Unit
+            
+            `e1` é a condição do while. enquanto `e1` for verdadeiro, o corpo `e2` é executado. 
+            `e1` portanto deve ser uma expressão booleana.
+            
+            `e2` é o corpo do while e deve ser um comando imperativo que é avaliado para `Unit`. 
+            
+            `while e1 do e2 end` é avaliado para `Unit`. *)
         | While (e1, e2) -> (
             let t1, env',  r1 = infer' e1 env r in
             let t2, env'', r2 = infer' e2 env' r1 in 
@@ -259,6 +323,22 @@ let infer (e: term) : tipo * type_inference = (
                 )
         )
 
+
+        (** e1 := e2
+            `e1`    deve ser um ponteiro para uma posição na memória.
+                    deve ser uma referência (Identifier `x`)
+                    portanto `e1` deve ser tipado `Reference t`
+                    isto é, uma referência ao tipo `t`, que é o tipo do objeto na memória.
+
+                    se um Identifier `x` aponta para um inteiro `VInteger 5` na posição `2` na memória
+                    então Identifier `x` : Reference Int
+            
+            `e2`    deve ser um termo que seja avaliado para um `t`.
+
+            `e1 := e2` é um comando imperativo que é avaliado para Unit; seu tipo é Unit.
+
+            `e1` : ref t ∧ `e2` : t => `e1` := `e2` : Unit
+            *)
         | Assignment (e1, e2) ->(
             let t1, env', r1 = infer' e1 env r in
             let t2, env'', r2 = infer' e2 env' r1 in
@@ -277,6 +357,18 @@ let infer (e: term) : tipo * type_inference = (
                 )
         )
 
+        (**
+            let x : t = e1 in e2
+            
+            `x` é o nome de um identificador (Identifier `x`)
+            `t` é o tipo desta variável, e que deve ser idêntico ao tipo de `e1`
+            `e1` é uma expressão cujo valor queremos associar ao `Identifier x`
+            `e2` é onde queremos usar o identificador, isto é, iremos substituir toda
+                    ocorrência de `Identifier x` pelo valor de `e1` em `e2`
+                    
+            então
+            
+            `e1` : t ∧ `e2` : t' => `let x = e1 in e2*)
         | Let (x, t, e1, e2) -> (
             let t1, env', r1 = infer' e1 env r in
             let env'' = (x, t) :: env' in
@@ -288,6 +380,17 @@ let infer (e: term) : tipo * type_inference = (
                     } :: r2)
         )
 
+        (**
+            new e
+            
+            `e` : t => `new e` : Reference(t)
+            
+            A expressão `new e` busca criar uma nova referência ao termo `e`
+            Quando usado em uma atribuição
+                `e1 := new e`
+            
+            `e1` será um ponteiro para uma referência ao termo `e`
+        *)
         | New e -> (
             let t, env', r' = infer' e env r in
             (Reference t,  env', {
@@ -297,6 +400,14 @@ let infer (e: term) : tipo * type_inference = (
                 } :: r')
         )
 
+        (**
+            `!e`
+            
+            `e` : Reference(t) => `!e` : t
+
+            Se `e` for um ponteiro para um termo de tipo `t`
+            então `!e` será um termo de tipo `t`. (pois `!e` é o valor que `e` aponta etc)
+        *)
         | Dereference e -> (
             let t, env', r' = infer' e env r in
             (match t with
@@ -313,12 +424,22 @@ let infer (e: term) : tipo * type_inference = (
             )
         )
 
+
+        (** Termo Unit é de tipo Unit. *)
         | Unit -> (Unit, env, {
             name = "T-Unit";
             pre = string_of_env env ^ " ⊢ '" ^ ast_of_term e ^ "' : Unit";
             post = string_of_env env ^ " ⊢ '" ^ ast_of_term e ^ "' : Unit";
             } :: r)
 
+        (**
+            `e1; e2
+            Sequência de expressões
+            
+            `e1` deve ser um comando imperativo que seja avaliado para Unit.
+            `e2` deve ser um termo de tipo `t`, então
+            
+            `e1; e2 : t`, pois `e1; e2 -> Unit, e2 -> e2` na avaliação. *)
         | Sequence (e1, e2) -> (
             let t1, env', r1 = infer' e1 env r in
             let t2, env'', r2 = infer' e2 env' r1 in
@@ -336,7 +457,12 @@ let infer (e: term) : tipo * type_inference = (
                 )
         )
 
-
+        
+        (** Localização l na memória
+            Se `l` for um local na memória, então `l` : Reference(t)
+            
+            `l` : Reference(t)
+        *)
         | Location l -> (
             let t, env', r' = infer' e env r in
             (match t with
